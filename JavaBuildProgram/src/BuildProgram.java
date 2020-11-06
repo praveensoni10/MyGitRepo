5794add6c4467986f062c12173cccbfc06e01cda
praveensoni10 <praveen.soni.10@gmail.com>
2020-11-06 21:42:59 +0530

    test

---
+++ JavaBuildProgram/src/BuildProgram.java
@@ -0,0 +1,14 @@
+public class BuildProgram 
+{
+
+ public static void main(String[] args)
+	    {
+	   
+	        System.out.println("Your details are as below: ");
+	        System.out.println("Name: xyz");
+	        System.out.println("Gender: Male");
+	        System.out.println("Age: 75");
+	        System.out.println("Mobile Number: 55447788");
+	        System.out.println("CGPA: 87");
+	    }
+	}
